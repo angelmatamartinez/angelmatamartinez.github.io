2b7e27b8cc3837864d21d8412edf8f82ded6bdf7
Angel Mata Martinez <angelmata1974@gmail.com>
2025-03-24 16:45:22 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://angelmatamartinez.github.io',
     integrations: [
         mdx(),
         sitemap(),
