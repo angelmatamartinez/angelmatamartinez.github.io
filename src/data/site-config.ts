3616b6600bf4a52e32a1b3dd3a1454738afa12a7
Angel Mata Martinez <angelmata1974@gmail.com>
2025-03-25 09:39:27 -0400

    Add diploy configuration

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -76,16 +76,8 @@ const siteConfig: SiteConfig = {
             text: 'Terms',
             href: '/terms'
         },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
     ],
     socialLinks: [
-        {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
-        },
         {
             text: 'Instagram',
             href: 'https://instagram.com/'
@@ -96,8 +88,8 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Hola & Bienvenido a mi portafolio!',
+        text: "Yo soy **Angel Mata**, soy desarrollador de aplicaciones de escritorio pero tambien desarrollo paginas y aplicaciones web en varios lenguajes y framework. Aparte de desarrollador tambien soy sysadmin con suficientes conocimientos en Linux y Windows server, me dedico tambien a investigar nuevas tecnologías y todo lo nuevo que sale en el mundo tecnológico.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
